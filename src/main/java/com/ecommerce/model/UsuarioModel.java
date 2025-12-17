f3abd3d6411ba78c5d0c4c68724aa672fb5fe2ad
rafadevgg <rafaelpc5671@gmail.com>
2025-12-17 17:37:55 -0300

    feat: Criando as classes entidades!

---
+++ src/main/java/com/ecommerce/model/UsuarioModel.java
@@ -0,0 +1,49 @@
+package com.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "TB_USUARIO")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
+public class UsuarioModel {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CDUSUARIO")
+    private Long cdUsuario;
+
+    @Column(name = "NMUSUARIO")
+    private String nmUsuario;
+
+    @Column(name = "DSEMAIL")
+    private String dsEmail;
+
+    @Column(name = "DSSENHA")
+    private String dsSenha;
+
+    @Column(name = "DSCPF")
+    private String dsCpf;
+
+    @Column(name = "DSTELEFONE")
+    private String dsTelefone;
+
+    @CreationTimestamp
+    @Column(name = "DTCRIACAO")
+    private LocalDateTime dtCriacao;
+
+    @LastModifiedDate
+    @Column(name = "DTATUALIZACAO")
+    private LocalDateTime dtAtualizacao;
+
+}
