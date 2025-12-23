f1cb7cdb956417fff62fbfeaed6f222a92b7c6d2
rafadevgg <rafaelpc5671@gmail.com>
2025-12-23 12:07:23 -0300

    fix: ImplÃementando Estoque!

--- src/main/java/com/ecommerce/model/EstoqueModel.java
+++ src/main/java/com/ecommerce/model/EstoqueModel.java
@@ -1,4 +1,39 @@
 package com.ecommerce.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "TB_ESTOQUE")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
 public class EstoqueModel {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CDESTOQUE")
+    private Long cdEstoque;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "CDPRODUTO", referencedColumnName = "CDPRODUTO")
+    private ProdutoModel cdProduto;
+
+    @Column(name = "QTESTOQUE")
+    private Integer qtEstoque;
+
+    @Column(name = "QTRESERVADO")
+    private Integer qtReservado;
+
+    @LastModifiedDate
+    @Column(name = "DTATUALIZACAO")
+    private LocalDateTime dtAtualizacao;
+
 }
