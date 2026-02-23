af9d02cf74206fe385cf868f400c8ba1b0db5333
rafadevgg <rafaelpc5671@gmail.com>
2026-02-23 11:27:26 -0300

    Fix: configs para Front-end!

--- src/main/java/com/ecommerce/repository/EnderecoRepository.java
+++ src/main/java/com/ecommerce/repository/EnderecoRepository.java
@@ -3,4 +3,12 @@ package com.ecommerce.repository;
 import com.ecommerce.model.EnderecoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EnderecoRepository extends JpaRepository<EnderecoModel, Long> {}
+import java.util.List;
+import java.util.Optional;
+
+public interface EnderecoRepository extends JpaRepository<EnderecoModel, Long> {
+
+    List<EnderecoModel> findByUsuario_CdUsuario(Long cdUsuario);
+    Optional<EnderecoModel> findByUsuario_CdUsuarioAndStPadraoTrue(Long cdUsuario);
+
+}
